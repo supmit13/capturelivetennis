e89dfec4867da87bde7d090ce30fed28734f92bc
supmit13 <supmit@gmail.com>
2022-07-09 18:06:45 +0530

    Webapp list, edit, delete and save functionality implemented

--- feedmanager/feedman/urls.py
+++ feedmanager/feedman/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('editfeed/', views.editfeed, name='editfeed'),
     path('deletefeed/', views.deletefeed, name='deletefeed'),
     path('savefeed/', views.savefeed, name='savefeed'),
+    path('searchfeed/', views.searchfeed, name='searchfeed'),
+    path('settings/', views.settings, name='settings'),
+    path('sendmail/', views.sendmail, name='sendmail'),
 ]
 
--- feedmanager/feedman/views.py
+++ feedmanager/feedman/views.py
@@ -4,6 +4,7 @@ import shutil
 import string, random
 import urllib.parse
 import urllib
+import simplejson as json
 
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
@@ -22,24 +23,212 @@ from django.http import FileResponse
 
 @login_required(login_url='/feedauth/showlogin/')
 def listfeeds(request):
-    return HttpResponse("")
+    if request.method != 'GET':
+        return HttpResponse("Invalid method of call")
+    context = {}
+    username = ""
+    if request.user.is_authenticated:
+        username = request.user.username
+    else:
+        return HttpResponseRedirect("/feedauth/showlogin/")
+    context['username'] = username
+    context['pagetitle'] = "Feeds List"
+    feedslist = []
+    allfeedsqset = Feed.objects.filter(deleted=False)
+    for feedobj in allfeedsqset:
+        d = {}
+        d['title'] = feedobj.feedtitle
+        d['fid'] = feedobj.id
+        d['player1'] = feedobj.feedeventteam1
+        d['player2'] = feedobj.feedeventteam2
+        d['matchdate'] = feedobj.feedstart
+        d['matchtype'] = feedobj.eventtype
+        d['result'] = feedobj.eventresult
+        d['status'] = feedobj.feedstatus # If this is a live event, we display it in green.
+        feedslist.append(d)
+    context['feedslist'] = feedslist
+    template = loader.get_template('feedslisting.html')
+    return HttpResponse(template.render(context, request))
 
 
 @login_required(login_url='/feedauth/showlogin/')
 def editfeed(request):
-    pass
+    if request.method != 'POST':
+        return HttpResponse(json.dumps({'error' : 'Invalid method of call'}))
+    if not request.user.is_authenticated:
+        return HttpResponse(json.dumps({'error' : 'user is not authenticated'}))
+    userid = request.user.id
+    feedid = ""
+    requestbody = str(request.body)
+    bodycomponents = requestbody.split("&")
+    requestdict = {}
+    for comp in bodycomponents:
+        compparts = comp.split("=")
+        if compparts.__len__() > 1:
+            compparts[0] = compparts[0].replace("b'", "")
+            requestdict[compparts[0]] = urllib.parse.unquote(compparts[1])
+    if 'feedid' not in requestdict.keys():
+        message = "Could not find feed Id as an argument of the request"
+        return HttpResponse(json.dumps({'error' : message}))
+    feedid = requestdict['feedid']
+    feedobj = None
+    try:
+        feedobj = Feed.objects.get(id=int(feedid))
+    except:
+        message = "Feed ID %s error: %s"%(feedid, sys.exc_info()[1].__str__())
+        return HttpResponse(json.dumps({'error' : message}))
+    feed = {}
+    feed['title'] = feedobj.feedtitle
+    feed['fid'] = feedobj.id
+    feed['player1'] = feedobj.feedeventteam1
+    feed['player2'] = feedobj.feedeventteam2
+    startdate = str(feedobj.feedstart).split("+")[0].replace(" ", "T").replace("Z", "")
+    feed['startdate'] = startdate
+    enddate = str(feedobj.feedend).split("+")[0].replace(" ", "T").replace("Z", "")
+    feed['enddate'] = enddate
+    if enddate == "None":
+        feed['enddate'] = ""
+    feed['eventtype'] = feedobj.eventtype
+    feed['result'] = feedobj.eventresult
+    feed['status'] = feedobj.feedstatus
+    feed['deleted'] = feedobj.deleted
+    feed['feedpath'] = feedobj.feedpath
+    feed['resultoptions'] = [feedobj.feedeventteam1, feedobj.feedeventteam2, "tie", "unknown"]
+    feed['error'] = ""
+    feedstr = json.dumps(feed)
+    return HttpResponse(feedstr)
 
 
 @login_required(login_url='/feedauth/showlogin/')
 @csrf_protect
 def savefeed(request):
-    pass
+    if request.method != 'POST':
+        return HttpResponse("Invalid method of call")
+    if not request.user.is_authenticated:
+        return HttpResponse("Your session is invalid. Please login to perform this operation")
+    requestbody = str(request.body)
+    requestdict = urllib.parse.parse_qs(requestbody)
+    #print(requestdict)
+    # Convert all double quotes in input to single quotes... pain! Also, urllib.parse.parse_qs leaves "b'" in the keys in some cases.
+    newrequestdict = {}
+    for k in requestdict.keys():
+        newk = k.replace("b'", "")
+        newrequestdict[newk] = requestdict[k][0].replace('"', "'")
+    # Now save the data.
+    feedid, feedtitle, feedplayer1, feedplayer2, eventtype, feedpath, feedstart, feedend, deleted, feedresult, feedstatus = -1, "", "", "", "", "", "", "", 0, "", ""
+    print(newrequestdict.keys())
+    if 'feedid' in newrequestdict.keys():
+        feedid = newrequestdict['feedid']
+    if 'title' in newrequestdict.keys():
+        feedtitle = newrequestdict['title']
+    if 'player1' in newrequestdict.keys():
+        feedplayer1 = newrequestdict['player1']
+    if 'player2' in newrequestdict.keys():
+        feedplayer2 = newrequestdict['player2']
+    if 'eventtype' in newrequestdict.keys():
+        eventtype = newrequestdict['eventtype']
+    if 'feedpath' in newrequestdict.keys():
+        feedpath = newrequestdict['feedpath']
+    if 'startdate' in newrequestdict.keys():
+        feedstart = newrequestdict['startdate']
+    if 'enddate' in newrequestdict.keys():
+        feedend = newrequestdict['enddate']
+    if 'deleted' in newrequestdict.keys():
+        deleted = newrequestdict['deleted']
+    if 'result' in newrequestdict.keys():
+        feedresult = newrequestdict['result']
+    if 'status' in newrequestdict.keys():
+        feedstatus = newrequestdict['status']
+    # Convert dates to python datetime values
+    mszPattern = re.compile("\.\d+Z", re.IGNORECASE)
+    feedstart = mszPattern.sub("", feedstart)
+    feedend = mszPattern.sub("", feedend)
+    #print(feedstart)
+    if feedstart != "":
+        feedstart = datetime.strptime(feedstart, "%Y-%m-%dT%H:%M:%S")
+    if feedend != "":
+        feedend = datetime.strptime(feedend, "%Y-%m-%dT%H:%M:%S")
+    feedobj = None
+    try:
+        feedobj = Feed.objects.get(id=feedid)
+    except:
+        message = "Could not identify the feed object (%s) uniquely"%feedid
+        return HttpResponse(message)
+    feedobj.feedtitle = feedtitle
+    feedobj.feedeventteam1 = feedplayer1
+    feedobj.feedeventteam2 = feedplayer2
+    feedobj.feedstart = feedstart
+    feedobj.feedend = feedend
+    feedobj.eventtype = eventtype
+    feedobj.eventresult = feedresult
+    feedobj.feedstatus = feedstatus
+    feedobj.feedpath = feedpath
+    if int(deleted) == 0:
+        feedobj.deleted = False
+    else:
+        feedobj.deleted = True
+    feedobj.updatetime = datetime.now()
+    feedobj.updateuser = request.user
+    try:
+        feedobj.save()
+    except:
+        message = "Error occurred while saving the changes: %s"%sys.exc_info()[1].__str__()
+    message = 'Successfully saved changes to the feed'
+    return HttpResponse(message)
 
 
 @login_required(login_url='/feedauth/showlogin/')
 @csrf_protect
 def deletefeed(request):
+    if request.method != 'POST':
+        return HttpResponse("Invalid method of call")
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect("/feedauth/showlogin/")
+    userid = request.user.id
+    feedid = ""
+    requestbody = str(request.body)
+    bodycomponents = requestbody.split("&")
+    requestdict = {}
+    for comp in bodycomponents:
+        compparts = comp.split("=")
+        if compparts.__len__() > 1:
+            compparts[0] = compparts[0].replace("b'", "")
+            requestdict[compparts[0]] = urllib.parse.unquote(compparts[1])
+    if 'feedid' not in requestdict.keys():
+        message = "Could not find feed Id as an argument of the request"
+        return HttpResponse(message)
+    feedid = requestdict['feedid']
+    feedobj = None
+    try:
+        feedobj = Feed.objects.get(id=int(feedid))
+    except:
+        message = "Feed ID %s error: %s"%(feedid, sys.exc_info()[1].__str__())
+        return HttpResponse(message)
+    feedobj.deleted = True
+    feedobj.save()
+    message = "The selected feed was successfully deleted"
+    return HttpResponse(message)
+    
+
+
+@login_required(login_url='/feedauth/showlogin/')
+def searchfeed(request):
     pass
+
+
+@login_required(login_url='/feedauth/showlogin/')
+@csrf_protect
+def settings(request):
+    pass
+
+
+@login_required(login_url='/feedauth/showlogin/')
+@csrf_protect
+def sendmail(request):
+    pass
+
+
+
 
 
 

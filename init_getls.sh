a2c1dc755e1453153a090cea978efc8d162f0383
supmit13 <supmit@gmail.com>
2022-07-14 03:28:15 +0530

    Added comments in shell script

--- init_getls.sh
+++ init_getls.sh
@@ -7,14 +7,16 @@
 # @reboot  /home/user/init_getls.sh
 
 # Path to python interpreter in (virtual) environment
-pypath="/home/user/videocapture/pyenv/bin/python3"
+pypath="/home/supmit/work/capturelivefeed/videoenv/bin/python"
 
 # Path to getlivestream.py
-getlspath="/home/user/videocapture/getlivestream.py"
+getlspath="/home/supmit/work/capturelivefeed/getlivestream.py"
 
 # execute getlivestream.py
 `nohup $pypath $getlspath "https://live.itftennis.com/en/live-streams/" &`
 
+# Provide execute permissions to this script:
+# chmod 755 /home/supmit/work/capturelivefeed/init_getls.sh
 # Done!
 # Dev: supmit
 
